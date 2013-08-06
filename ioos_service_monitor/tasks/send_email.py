2f2ac20684f6c5b78f7b6c8d71b62a5ebed8bff4
Dave Foster <dfoster@asascience.com>
2013-08-06 14:57:33 -0400

    Add status change email after ping task

--- ioos_service_monitor/tasks/send_email.py
+++ ioos_service_monitor/tasks/send_email.py
@@ -1,9 +1,35 @@
 from flask.ext.mail import Message
-from ioos_service_monitor import app, mail
+from ioos_service_monitor import db, app, mail
+from flask import render_template
 
-def send(subject, sender, recipients, text_body, html_body):
+def send(subject, recipients, cc_recipients, text_body, html_body):
+    # sender comes from MAIL_DEFAULT_SENDER in env
+    msg = Message(subject, recipients=recipients, cc=cc_recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
+
+def send_service_down_email(service_id):
     with app.app_context():
-        msg = Message(subject, sender=sender, recipients=recipients)
-        msg.body = text_body
-        msg.html = html_body
-        mail.send(msg)
+        kwargs = {'service' : db.Service.find_one({'_id':service_id}),
+                  'stat'    : db.Stat.find_one({'service_id':service_id}, sort=[('created',-1)]),
+                  'last_success_stat' : db.Stat.find_one({'service_id':service_id, 'operational_status':1}, sort=[('created',-1)]) }
+        kwargs['status'] = kwargs['stat'].operational_status
+
+        subject = "Service Status Alert (%s): %s (%s)" % ("UP" if kwargs['status'] else "DOWN", kwargs['service'].name, kwargs['service'].service_type)
+
+        text_template = render_template("service_status_changed.txt", **kwargs)
+        html_template = render_template("service_status_changed.html", **kwargs)
+
+        to_addresses = ["ioos-dmac@asa.flowdock.com"]
+        if app.config.get('DEBUG') == False and kwargs['service'].contact is not None:
+            to_addresses = kwargs['service'].contact.split(",")
+
+        cc_addresses = ["ioos.catalog@noaa.gov"] if app.config.get('DEBUG') == False else None
+
+        send(subject,
+             to_addresses,
+             cc_addresses,
+             text_template,
+             html_template)
+
--- ioos_service_monitor/tasks/stat.py
+++ ioos_service_monitor/tasks/stat.py
@@ -1,10 +1,22 @@
-from ioos_service_monitor import app, db
+from ioos_service_monitor import app, db, scheduler
 from bson import ObjectId
+from ioos_service_monitor.tasks.send_email import send_service_down_email
 
 def ping_service_task(service_id):
     with app.app_context():
+        # get last for this service
+        last_stat = db.Stat.find_one({'service_id':ObjectId(service_id)}, sort=[('created',-1)])
+
         stat = db.Stat()
         stat.service_id=ObjectId(service_id)
         stat.ping_service()
         stat.save()
+
+        if last_stat and last_stat.operational_status != stat.operational_status:
+            scheduler.schedule(
+                scheduled_time=datetime.now(),
+                func=send_service_down_email,
+                repeat=1,
+                result_ttl=2000
+            )
 
