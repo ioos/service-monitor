c8903acdd8a6056d2bd20cbb13548b78e55e1832
Kyle Wilcox <kwilcox@asascience.com>
2013-08-13 16:14:43 -0400

    Add daily email reporting to the 'regulate' task

--- ioos_service_monitor/tasks/regulator.py
+++ ioos_service_monitor/tasks/regulator.py
@@ -1,16 +1,38 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from bson.objectid import ObjectId
 
 from ioos_service_monitor import app, db, scheduler
 
 from ioos_service_monitor.tasks.stat import ping_service_task
 from ioos_service_monitor.tasks.reindex_services import reindex_services
+from ioos_service_monitor.tasks.send_email import send_daily_report_email
 
 def regulate():
     with app.app_context():
 
         # Get function and args of 
         jobs = scheduler.get_jobs()
+
+        # Make sure a daily report job is running
+        daily_email_jobs = [job for job in jobs if job.func == send_daily_report_email]
+        if len(daily_email_jobs) > 1:
+            # Cancel all but the first daily email job
+            for j in daily_email_jobs[1:]:
+                scheduler.cancel(j)
+        elif len(daily_email_jobs) < 1:
+            # Run today at 3am (if it is between midnight and 3am)
+            runat = datetime.now().replace(hour=3, minute=0, second=0, microsecond=0)
+            if datetime.now() > runat:
+                # Run tomorrow at 3am (it is already past 3am)
+                runat = runat + timedelta(days=1)
+
+            scheduler.schedule(
+                scheduled_time=runat,           # Time for first execution
+                func=send_daily_report_email,   # Function to be queued
+                interval=86400,                 # Time before the function is called again, in seconds (86400 == 1 day)
+                repeat=None,                    # Repeat this number of times (None means repeat forever)
+                result_ttl=100000                # How long to keep the results, in seconds
+            )
 
         # Make sure a service update job is running
         reindex_services_jobs = [job for job in jobs if job.func == reindex_services]
