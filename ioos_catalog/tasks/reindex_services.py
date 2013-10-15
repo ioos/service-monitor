90bbb17e8360b1010ac86c4afb62bf3283332306
Kyle Wilcox <kwilcox@asascience.com>
2013-10-15 13:15:45 -0400

    Fix typo. SCOOS -> SCCOOS.

--- ioos_catalog/tasks/reindex_services.py
+++ ioos_catalog/tasks/reindex_services.py
@@ -19,7 +19,7 @@ def reindex_services():
                         'NANOOS'    :   'C6F4754B-30DC-459E-883A-2AC79DA977AB',
                         'NERACOOS'  :   'E13C88D9-3FF3-4232-A379-84B6A1D7083E',
                         'PacIOOS'   :   '78C0463E-2FCE-4AB2-A9C9-6A34BF261F52',
-                        'SCOOS'     :   '20A3408F-9EC4-4B36-8E10-BBCDB1E81BDF',
+                        'SCCOOS'    :   '20A3408F-9EC4-4B36-8E10-BBCDB1E81BDF',
                         'SECOORA'   :   'E796C954-B248-4118-896C-42E6FAA6EDE9',
                         'GLOS'      :   'E4A9E4F4-78A4-4BA0-B653-F548D74F68FA',
                         'CARICOOS'  :   '0C4CA8A6-5967-4590-BFE0-B8A21CD8BB01' }
