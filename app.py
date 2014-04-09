1e6c0bd2aa41fab575f11c213dbaac0f5c8ce9b1
Dave Foster <dfoster@asascience.com>
2014-04-09 17:00:06 -0400

    Commented out profiler middleware for dev mode

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from ioos_catalog import app
 import os
+from werkzeug.contrib.profiler import ProfilerMiddleware
 
 if os.environ.get('APPLICATION_SETTINGS') == 'development.py':
+    #app.config['PROFILE']=True
+    #app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[10])
+
     app.run(host="0.0.0.0", port=3000, debug=True)
