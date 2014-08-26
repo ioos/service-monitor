7d8d9f2ac44b5aa0f4094ca9aba0d0fd6914ad0e
Luke Campbell <luke.s.campbell@gmail.com>
2014-08-26 13:00:58 -0400

    Adds configurable port to deployment(s)

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ from werkzeug.contrib.profiler import ProfilerMiddleware
 if os.environ.get('APPLICATION_SETTINGS') == 'development.py':
     #app.config['PROFILE']=True
     #app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[10])
+    host = os.environ.get('APPLICATION_HOST', '0.0.0.0')
+    port = int(os.environ.get('APPLICATION_PORT', 3000))
 
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host=host, port=port, debug=True)
