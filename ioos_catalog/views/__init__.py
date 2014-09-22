c4f99a43e1929ebb620b7ff59c2c8a7da8771680
Luke Campbell <luke.s.campbell@gmail.com>
2014-09-22 12:12:25 -0400

    Creates landing
    - New landing page, replaces the dashboard as an index page.
    - Moves the dashboard to a new endpoint /dashboard
    
    Fixes #83

--- ioos_catalog/views/__init__.py
+++ ioos_catalog/views/__init__.py
@@ -1,2 +1,2 @@
-from ioos_catalog.views import index, service, dataset, amap, help, metadata, ra, gliders, by_the_numbers, featured_maps
+from ioos_catalog.views import dashboard, service, dataset, amap, help, metadata, ra, gliders, by_the_numbers, featured_maps, landing
 
