c55ddfb2362511d92c51d47c5703a7329abb8823
Benjamin Adams <badams@asascience.com>
2015-01-26 14:39:15 -0500

    Adds basic dataset/service metrics to database
    Partial implementation of #340.  Includes services by type, services by
    RA, and datasets from RA.  Total datasets/services not included since
    they can be computed from the aggregates of the aforementioned
    statistics.

--- ioos_catalog/models/__init__.py
+++ ioos_catalog/models/__init__.py
@@ -1,1 +1,3 @@
-from ioos_catalog.models import service, stat, dataset, ping_latest, ping_archive, metadata, migrations, harvests
+from ioos_catalog.models import (service, stat, dataset, metric_counts,
+                                 ping_latest, ping_archive, metadata,
+                                 migrations, harvests)
